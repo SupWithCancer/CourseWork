852252b8c1d0d17aa4a2987b6535ac0e9488a83c
supwithcancer <54910786+SupWithCancer@users.noreply.github.com>
2021-12-19 20:06:56 +0200

    Add files via upload

---
+++ BusinessLogicLayer/Services/FilmService.cs
@@ -0,0 +1,94 @@
+﻿using AutoMapper;
+using BusinessLogicLayer.DTOs;
+using BusinessLogicLayer.Interfaces;
+using DataAccessLayer.Models;
+using DataAccessLayer;
+using System.Linq.Expressions;
+
+namespace BusinessLogicLayer.Services
+{
+    public class FilmService : IFilmService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+
+        public FilmService(IUoWFactory uowFactory, IMapper mapper)
+        {
+            _unitOfWork = uowFactory.CreateUoW();
+            _mapper = mapper;
+        }
+
+        public async Task<FilmDTO> GetFilmAsync(int id)
+        {
+            var film = await _unitOfWork.Film.GetByIdAsync(id);
+
+            return _mapper.Map<FilmDTO>(film);
+        }
+
+        public async Task<List<FilmDTO>> GetFilmsAsync()
+        {
+            var films = await _unitOfWork.Film.GetAllAsync();
+
+            return _mapper.Map<List<FilmDTO>>(films);
+        }
+
+        //public async Task<List<FilmDTO>> GetFilmsByGenreIdAsync(int categoryId)
+        //{
+        //    var recipes = await _unitOfWork.Recipes.GetWhereAsync(d => d.CategoryId == categoryId);
+
+        //    return _mapper.Map<List<RecipeDTO>>(recipes);
+        //}
+
+        //public async Task<List<RecipeDTO>> GetRecipesByCuisineIdAsync(int cuisineId)
+        //{
+        //    var recipes = await _unitOfWork.Recipes.GetWhereAsync(d => d.CuisineId == cuisineId);
+
+        //    return _mapper.Map<List<RecipeDTO>>(recipes);
+        //}
+
+        public async Task<bool> AnyFilmsAsync(Expression<Func<Film, bool>> expression)
+        {
+            return await _unitOfWork.Film.AnyExistingAsync(expression);
+        }
+
+        public async Task PostFilmAsync(FilmDTO film)
+        {
+            await _unitOfWork.Film.AddAsync(_mapper.Map<Film>(film));
+            await _unitOfWork.SaveAsync();
+        }
+
+        public async Task PutFilmAsync(int id, FilmDTO film)
+        {
+            await _unitOfWork.Film.UpdateAsync(_mapper.Map<Film>(film));
+            await _unitOfWork.SaveAsync();
+        }
+
+        public async Task DeleteFilmAsync(int id)
+        {
+            var film = await _unitOfWork.Film.GetByIdAsync(id);
+            if (film == null) throw new KeyNotFoundException();
+
+            await _unitOfWork.Film.DeleteAsync(film);
+            await _unitOfWork.SaveAsync();
+        }
+
+        private bool _disposed = false;
+
+        public virtual void Dispose(bool disposing)
+        {
+            if (!_disposed)
+            {
+                if (disposing)
+                    _unitOfWork.Dispose();
+
+                _disposed = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
---
+++ BusinessLogicLayer/Services/UserService.cs
@@ -0,0 +1,112 @@
+﻿using AutoMapper;
+using System.Security.Cryptography;
+using System.Security.Claims;
+using System.IdentityModel.Tokens.Jwt;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.Extensions.Configuration;
+using DataAccessLayer.Models;
+using DataAccessLayer;
+using BusinessLogicLayer.DTOs;
+using BusinessLogicLayer.Interfaces;
+
+namespace BusinessLogicLayer.Services
+{
+    public class UserService : IUserService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+        private readonly IConfiguration _configuration;
+
+        public UserService(IUoWFactory uowFactory, IMapper mapper, IConfiguration configuration)
+        {
+            _unitOfWork = uowFactory.CreateUoW();
+            _mapper = mapper;
+            _configuration = configuration;
+        }
+
+        public async Task<List<UserInfoDTO>> GetUsersInfoAsync()
+        {
+            var usersInfo = await _unitOfWork.User.GetAllAsync();
+
+            return _mapper.Map<List<UserInfoDTO>>(usersInfo);
+        }
+
+        public async Task<UserInfoDTO> GetUserInfoByIdAsync(int id)
+        {
+            var userInfo = await _unitOfWork.User.GetByIdAsync(id);
+
+            return _mapper.Map<UserInfoDTO>(userInfo);
+        }
+
+        public async Task<LoginRes?> AuthenticateAsync(LoginReq request)
+        {
+            var user = await _unitOfWork.User.GetFirstOrDefaultAsync(x => x.Email == request.Email);
+
+            if (user == null) return null;
+            
+            if (!VerifyPasswordHash(request.Password, user.PasswordHash, user.PasswordSalt))
+                return null;
+
+            var token = GenerateJsonWebToken(user);
+
+            return new LoginRes(user, token); ;
+        }
+
+        public async Task<LoginRes?> RegisterAsync(UserDTO userModel)
+        {
+            CreatePasswordHash(userModel.Password, out byte[] passwordHash, out byte[] passwordSalt);
+
+            var user = new User
+            {
+                Id = userModel.Id,
+                Email = userModel.Email,
+                Name = userModel.Name, 
+                PasswordHash = passwordHash,
+                PasswordSalt = passwordSalt
+            };
+
+            await _unitOfWork.User.AddAsync(user);
+            await _unitOfWork.SaveAsync();
+
+            return await AuthenticateAsync(new LoginReq
+                         {
+                             Email = userModel.Email,
+                             Password = userModel.Password
+                         });
+        }
+
+        private string GenerateJsonWebToken(User user)
+        {
+            var claims = new List<Claim>() { new Claim(ClaimTypes.Name, user.Email) };
+
+            var key = new SymmetricSecurityKey(
+                System.Text.Encoding.UTF8.GetBytes(
+                    _configuration.GetSection("Secret").Value));
+
+            var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+            var token = new JwtSecurityToken(
+                            claims: claims,
+                            signingCredentials: credentials,
+                            expires: DateTime.Now.AddDays(1));
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+        private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            using var hmac = new HMACSHA512();
+
+            passwordSalt = hmac.Key;
+            passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+        }
+
+        private bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt)
+        {
+            using var hmac = new HMACSHA512(passwordSalt);
+
+            var computedHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            return computedHash.SequenceEqual(passwordHash);
+        }
+    }
+}
