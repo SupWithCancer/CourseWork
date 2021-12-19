380790d21ad33cc66f560028524fac0a7ea18e4d
supwithcancer <54910786+SupWithCancer@users.noreply.github.com>
2021-12-19 20:09:18 +0200

    Add files via upload

---
+++ BusinessLogicLayer/Configuration/MappingProfile.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using DataAccessLayer.Models;
+using BusinessLogicLayer.DTOs;
+
+namespace BusinessLogicLayer.Configuration
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<User, UserDTO>();
+            CreateMap<UserDTO, User>();
+
+            CreateMap<User, UserInfoDTO>();
+            CreateMap<UserInfoDTO, User>();
+
+            CreateMap<Film, FilmDTO>();
+            CreateMap<FilmDTO, Film>();
+        }
+    }
+}
